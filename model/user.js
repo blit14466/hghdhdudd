60d61367d3f0d66d2ca4fbe7839ca370b7a0aa51
Glitch (military-berrying-titanium) <none>
2025-05-02 12:56:03 +0000

    👑🪚 Updated with Glitch

---
+++ model/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+// Password comparison method
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
